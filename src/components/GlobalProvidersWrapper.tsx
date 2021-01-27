e6c8042a577095a1a7dc0bf12ffb11ce573a8ea2
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-01-27 02:16:17 -0300

    chore: added components folder base structure

--- src/components/GlobalProvidersWrapper.tsx
+++ src/components/GlobalProvidersWrapper.tsx
@@ -1,13 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React, { FC } from 'react';
 
-import { Main } from 'layouts/Main';
+import Main from 'layouts/Main';
 import theme from 'theme';
 
-export const GlobalProvidersWrapper: FC = ({ children }) => (
+const GlobalProvidersWrapper: FC = ({ children }) => (
   <Main>
     <ChakraProvider theme={theme}>
       {children}
     </ChakraProvider>
   </Main>
 );
+
+export default GlobalProvidersWrapper;
