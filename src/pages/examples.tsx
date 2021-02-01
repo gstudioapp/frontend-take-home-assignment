e5188c26742d1cc9dab8df9842237fd6b16b27f8
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-01 19:27:38 -0300

    feat: done the interface

---
+++ src/pages/examples.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { NextPage } from 'next';
+
+const Examples: NextPage = () => {
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  );
+};
+
+export default Examples;
---
+++ src/pages/themes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { NextPage } from 'next';
+
+const Themes: NextPage = () => {
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  );
+};
+
+export default Themes;
