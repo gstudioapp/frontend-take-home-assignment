b592e97d6f21c11fe9ae4a439aca68e83bb1c305
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-01 16:28:51 -0300

    feat: created the avatar component

--- src/components/UiAvatar/index.tsx
+++ src/components/UiAvatar/index.tsx
@@ -1,19 +1,44 @@
 import React, { FC } from 'react';
-import { Avatar, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { DotsVerticalSolid } from '@graywolfai/react-heroicons';
 
-const UiAvatar: FC = () => {
+interface IProps {
+  imageSrc: string;
+  userName: string;
+}
+
+const UiAvatar: FC<IProps> = ({ userName, imageSrc }) => {
   return (
-    <Flex alignItems="center">
-      <Avatar
-        size="sm"
-        name="Jair Junior"
+    <Flex justifyContent="space-between" alignItems="center">
+      <Flex alignItems="center" isTruncated={true}>
+        <Avatar
+          size="sm"
+          marginRight="0.75rem"
+          src={imageSrc}
+          name={userName}
+        />
+        <Text
+          fontSize="sm"
+          fontFamily="mono"
+          fontWeight="medium"
+          isTruncated={true}
+          width="100%"
+        >
+          {userName}
+        </Text>
+      </Flex>
+      <Box
+        width="1rem"
+        minWidth="1rem"
+        color="gray.500"
         cursor="pointer"
-        src="https://bit.ly/dan-abramov"
-        marginRight="0.75rem"
-      />
-      <Text fontFamily="mono" fontWeight="medium" fontSize="sm">
-        Jair Junior
-      </Text>
+        paddingY="0.3rem"
+        borderRadius="base"
+        transition="0.3s"
+        _hover={{ bg: 'gray.100', color: 'gray.600' }}
+      >
+        <DotsVerticalSolid />
+      </Box>
     </Flex>
   );
 };
