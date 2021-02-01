0ead419aa6e083ec775621bc07a16aa8f4306214
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-01 16:30:55 -0300

    feat: new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,10 @@
+export { default as UiAvatar } from './UiAvatar';
+export { default as UiCard } from './UiCard';
+export { default as UiCircularProgress } from './UiCircularProgress';
+export { default as UiDropdown } from './UiDropdown';
+export { default as UiIcon } from './UiIcon';
+export { default as UiLayout } from './UiLayout';
+export { default as UiPage } from './UiPage';
+export { default as UiProgress } from './UiProgress';
+export { default as UiSidebar } from './UiSidebar';
 export { default as GlobalProvidersWrapper } from './GlobalProvidersWrapper';
